ef3c1358b3c2154fadfaed7f9a29800435a81706
zyedibrahim <zyedibrahim77@gmail.com>
2023-03-15 13:30:21 +0530

    f

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import axios from "axios";
 // import request from "request-promise";
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config()
-import express from "express"; // "type": "module"
+import express from "express"; 
 import { MongoClient } from "mongodb";
 import cors from "cors"
 const MONGO_URL = process.env.MONGO_URL;
