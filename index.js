f3f83da2bb51994c25ae5424e10bb4cbb6822c74
zyedibrahim <zyedibrahim77@gmail.com>
2023-03-15 13:32:11 +0530

    f

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 import cheerio from "cheerio"
 import axios from "axios";
 // import request from "request-promise";
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
+// import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+// dotenv.config()
 import express from "express"; 
 import { MongoClient } from "mongodb";
 import cors from "cors"
